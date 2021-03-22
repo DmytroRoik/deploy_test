44ddbdb2ce44fd5a70fe835b9871589a5ae67ae3
DmytroRoik-Incora <dmytro.roik@incorainc.com>
2021-03-22 16:27:05 +0200

    finished redux

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,3 +1,22 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addProduct } from '../../state/actions/products';
+import { allProductsSelector } from '../../state/selectors/products';
+import { AppState } from '../../state/store';
 
-export const NotFound = () => <div onClick={() => {}}>Not Found Page</div>;
+export const NotFound = () => {
+  const products = useSelector<AppState>(allProductsSelector);
+  console.log(products);
+
+  const dispatch = useDispatch();
+
+  const addProductItem = () => {
+    dispatch(addProduct({ id: Date.now(), name: 'blablabla' }));
+  };
+
+  return (
+    <div>
+      <button onClick={addProductItem}>Add Product</button>
+    </div>
+  );
+};
